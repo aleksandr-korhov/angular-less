a23a84a868c25375ec6fe2f0646673436011fea6
aleksandr.korhov <aleksandr.korhov@devz.aitibiz.ru>
2017-08-16 20:38:58 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from "@angular/forms";
 
 
 import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UsersDataService } from './shared/users-data.service';
@@ -17,7 +16,7 @@ import { AdminService } from "./shared/admin.service";
 import { HighlightDirective } from './highlight.directive';
 
 /* Feature Modules */
-import { UsersModule } from './users-new/users.module';
+import { UsersModule } from './users/users.module';
 
 /* Routing Module */
 import { AppRoutingModule } from "./app-routing.module";
@@ -35,7 +34,6 @@ import { AppRoutingModule } from "./app-routing.module";
         TitleComponent,
         HighlightDirective,
         // HomeComponent,
-        // UsersComponent,
         // UserComponent,
         // NotFoundComponent,
         // CustomDatePipe,
