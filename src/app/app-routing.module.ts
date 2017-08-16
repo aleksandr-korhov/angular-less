cab982a6214124a4fc12cba27c2a358058c3806f
aleksandr.korhov <aleksandr.korhov@devz.aitibiz.ru>
2017-08-16 20:41:54 +0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { HomeComponent } from "./home/home.component";
+// import { HomeComponent } from "./home/home.component";
 import { UsersComponent } from "./users/users.component";
 // import { UserComponent } from "./user/user.component";
 // import { NotFoundComponent } from "./not-found/not-found.component";
